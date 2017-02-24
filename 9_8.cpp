889637f214a5b8da0388d509e4a0bbeaea66bf23
StephenWez <a18588430291@163.com>
2017-02-25 00:36:07 +0800

    2017-2-25

--- 9_8.cpp
+++ 9_8.cpp
@@ -1,14 +1,24 @@
 #include<iostream>
+#include<cmath>
+#include<iomanip>
 using namespace std;
 int main()
 {
-	int n,m;
+	int n;
 	cin>>n;
-	double m,ans1,max=0,min=100
+	double m[n],ans1,max=0,min=100,max_,min_,maxc=0,sum=0;
 	for (int i=0;i<n;i++) {
-		cin>>m;
-		if (m>max) max=m;
-		
+		cin>>m[i];
+		if (m[i]>max) {max=m[i];max_=i;}
+		if (m[i]<min) {min=m[i];min_=i;}
+		sum+=m[i];
 	}
+	sum=sum-max-min;
+	ans1=sum/(n*1.0-2);
+	for (int i=0;i<n;i++) {
+		if (abs(ans1-m[i])>maxc&&i!=max_&&min_!=i) maxc=abs(ans1-m[i]);
+	}
+	cout<<fixed<<setprecision(2)<<ans1<<' ';
+	cout<<fixed<<setprecision(2)<<maxc<<endl;
 	return 0;
 }
